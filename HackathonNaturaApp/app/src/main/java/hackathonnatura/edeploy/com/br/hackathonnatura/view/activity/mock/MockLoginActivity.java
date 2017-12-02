e57cfb1218d93abb33c3d2b48a82599f2bad0ae7
Victor Moraes <vicctormoraes@gmail.com>
2017-12-02 17:32:01 -0200

    mock login

--- HackathonNaturaApp/app/src/main/java/hackathonnatura/edeploy/com/br/hackathonnatura/view/activity/mock/MockLoginActivity.java
+++ HackathonNaturaApp/app/src/main/java/hackathonnatura/edeploy/com/br/hackathonnatura/view/activity/mock/MockLoginActivity.java
@@ -1,10 +1,12 @@
 package hackathonnatura.edeploy.com.br.hackathonnatura.view.activity.mock;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 
 import hackathonnatura.edeploy.com.br.hackathonnatura.R;
 import hackathonnatura.edeploy.com.br.hackathonnatura.view.activity.BaseActivity;
+import hackathonnatura.edeploy.com.br.hackathonnatura.view.activity.HomeActivity_;
 
 /**
  * Created by vcmoraes on 02/12/17.
@@ -15,5 +17,11 @@ public class MockLoginActivity extends BaseActivity {
     @AfterViews
     public void init() {
 
+    }
+
+    @Click(R.id.btn_login)
+    void onLogin() {
+        HomeActivity_.intent(this).start();
+        finish();
     }
 }
