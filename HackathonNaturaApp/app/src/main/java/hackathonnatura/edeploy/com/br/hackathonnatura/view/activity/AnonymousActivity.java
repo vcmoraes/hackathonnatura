8441a51c4df50cf2fefc4bceaebea37c4c4e365e
Sérgio Souza <sergio.souza@e-deploy.com.br>
2017-12-03 12:14:43 -0200

    Criação do dialog customizado

--- HackathonNaturaApp/app/src/main/java/hackathonnatura/edeploy/com/br/hackathonnatura/view/activity/AnonymousActivity.java
+++ HackathonNaturaApp/app/src/main/java/hackathonnatura/edeploy/com/br/hackathonnatura/view/activity/AnonymousActivity.java
@@ -1,13 +1,16 @@
 package hackathonnatura.edeploy.com.br.hackathonnatura.view.activity;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.widget.EditText;
 
 import hackathonnatura.edeploy.com.br.hackathonnatura.R;
+import hackathonnatura.edeploy.com.br.hackathonnatura.enums.DialogType;
 import hackathonnatura.edeploy.com.br.hackathonnatura.model.Consultora;
 import hackathonnatura.edeploy.com.br.hackathonnatura.model.UpdateList;
 import hackathonnatura.edeploy.com.br.hackathonnatura.sql.dao.ConsultoraDao;
 import hackathonnatura.edeploy.com.br.hackathonnatura.util.Constants;
+import hackathonnatura.edeploy.com.br.hackathonnatura.view.fragment.NaturaDialogFragment;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
@@ -34,22 +37,50 @@ public class AnonymousActivity extends BaseActivity {
     }
 
     private boolean validate() {
-        return true;
+        boolean result = true;
+
+        if (editTextName.getText().toString().length() == 0) {
+            result = false;
+        }
+
+        return result;
     }
 
     @Click(R.id.button_register)
     void onButtonRegister() {
 
         if (!this.validate()) {
+            showMessage("É necessário informar o nome da CN.", null, DialogType.ERROR);
+
             return;
         }
 
-        Consultora consultora = new Consultora();
-        consultora.setId("");
-        consultora.setNome(editTextName.getText().toString());
-        consultora.setTelefone(editTextPhoneNumber.getText().toString());
-        consultora.setDateCheckin(Calendar.getInstance().getTime());
-        consultoraDao.salvar(consultora);
-        post(new UpdateList());
+//        Consultora consultora = new Consultora();
+//        consultora.setId("");
+//        consultora.setNome(editTextName.getText().toString());
+//        consultora.setTelefone(editTextPhoneNumber.getText().toString());
+//        consultora.setDateCheckin(Calendar.getInstance().getTime());
+//        consultoraDao.salvar(consultora);
+//        post(new UpdateList());
+
+        showMessage(editTextName.getText().toString(), "9999", DialogType.SUCCESS);
+    }
+
+    void showMessage(String message, String code, DialogType dialogType) {
+        NaturaDialogFragment naturaDialog = new NaturaDialogFragment();
+
+        if (dialogType == DialogType.SUCCESS) {
+            naturaDialog.setCnCode(this, code);
+            naturaDialog.setCnName(this, message);
+        } else {
+            naturaDialog.setMessage(this, message, dialogType);
+        }
+        naturaDialog.setClickListener(new NaturaDialogFragment.ClickListener() {
+            @Override
+            public void onClick() {
+
+            }
+        });
+        naturaDialog.show(this.getFragmentManager(), "");
     }
 }
