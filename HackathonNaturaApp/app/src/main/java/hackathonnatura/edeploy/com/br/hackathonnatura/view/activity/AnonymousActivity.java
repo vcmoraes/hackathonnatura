c2fbf4bb404766bb704f78bca3c10661970aff20
Sérgio Souza <sergio.souza@e-deploy.com.br>
2017-12-03 13:43:48 -0200

    Criação do dialog customizado

--- HackathonNaturaApp/app/src/main/java/hackathonnatura/edeploy/com/br/hackathonnatura/view/activity/AnonymousActivity.java
+++ HackathonNaturaApp/app/src/main/java/hackathonnatura/edeploy/com/br/hackathonnatura/view/activity/AnonymousActivity.java
@@ -1,7 +1,5 @@
 package hackathonnatura.edeploy.com.br.hackathonnatura.view.activity;
 
-import android.content.Intent;
-import android.support.design.widget.TabLayout;
 import android.widget.EditText;
 
 import hackathonnatura.edeploy.com.br.hackathonnatura.R;
@@ -9,10 +7,7 @@ import hackathonnatura.edeploy.com.br.hackathonnatura.enums.DialogType;
 import hackathonnatura.edeploy.com.br.hackathonnatura.model.Consultora;
 import hackathonnatura.edeploy.com.br.hackathonnatura.model.UpdateList;
 import hackathonnatura.edeploy.com.br.hackathonnatura.sql.dao.ConsultoraDao;
-import hackathonnatura.edeploy.com.br.hackathonnatura.util.Constants;
 import hackathonnatura.edeploy.com.br.hackathonnatura.view.fragment.NaturaDialogFragment;
-
-import android.widget.EditText;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
@@ -20,11 +15,6 @@ import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
 import java.util.Calendar;
-
-import hackathonnatura.edeploy.com.br.hackathonnatura.R;
-import hackathonnatura.edeploy.com.br.hackathonnatura.model.Consultora;
-import hackathonnatura.edeploy.com.br.hackathonnatura.model.UpdateList;
-import hackathonnatura.edeploy.com.br.hackathonnatura.sql.dao.ConsultoraDao;
 
 @EActivity(R.layout.activity_anonymous)
 public class AnonymousActivity extends BaseActivity {
@@ -57,20 +47,22 @@ public class AnonymousActivity extends BaseActivity {
     void onButtonRegister() {
 
         if (!this.validate()) {
-            showMessage("É necessário informar o nome da CN.", null, DialogType.ERROR);
-
+            showMessage(getString(R.string.nome_cn_vazio), null, DialogType.ERROR);
             return;
         }
 
-//        Consultora consultora = new Consultora();
-//        consultora.setId("");
-//        consultora.setNome(editTextName.getText().toString());
-//        consultora.setTelefone(editTextPhoneNumber.getText().toString());
-//        consultora.setDateCheckin(Calendar.getInstance().getTime());
-//        consultoraDao.salvar(consultora);
-//        post(new UpdateList());
+        Consultora consultora = new Consultora();
 
-        showMessage(editTextName.getText().toString(), "9999", DialogType.SUCCESS);
+        String uuid = consultora.getUuid();
+
+        consultora.setId(uuid);
+        consultora.setNome(editTextName.getText().toString());
+        consultora.setTelefone(editTextPhoneNumber.getText().toString());
+        consultora.setDateCheckin(Calendar.getInstance().getTime());
+        consultoraDao.salvar(consultora);
+        post(new UpdateList());
+
+        showMessage(editTextName.getText().toString(), uuid, DialogType.SUCCESS);
     }
 
     void showMessage(String message, String code, DialogType dialogType) {
